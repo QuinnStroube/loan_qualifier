aab9adba4da2a7650626f34b3719c9e195efb990
Quinn <q.stroube@gmail.com>
2022-06-22 15:37:15 -0500

    Updated with dialog to prompt users to save loans as csv

--- loan_qualifier_app/app.py
+++ loan_qualifier_app/app.py
@@ -6,12 +6,14 @@ This is a command line application to match applicants with qualifying loans.
 Example:
     $ python app.py
 """
+import csv
 import sys
 import fire
 import questionary
 from pathlib import Path
 
 from qualifier.utils.fileio import load_csv
+from qualifier.utils.fileio import save_csv
 
 from qualifier.utils.calculators import (
     calculate_monthly_debt_ratio,
@@ -104,12 +106,21 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
 def save_qualifying_loans(qualifying_loans):
     """Saves the qualifying loans to a CSV file.
-#qwqwqww
+
     Args:
         qualifying_loans (list of lists): The qualifying bank loans.
     """
+
     # @TODO: Complete the usability dialog for savings the CSV Files.
     # YOUR CODE HERE!
+    save_qualifying_loans = questionary.confirm("Do you want to save the Qualifying Loans?").ask()
+    
+    csvpath = questionary.text("Enter a file path to save the qualifying loans:").ask()
+    csvpath = Path(csvpath)
+    if not csvpath.exists():
+        sys.exit(f"Oops! Can't find this path: {csvpath}")
+
+    return save_csv
 
 
 def run():
